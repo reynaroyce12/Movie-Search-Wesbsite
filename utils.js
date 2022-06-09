f559d710f4e79eca4d631938113d9dd4114f3765
Reyna Royce <reynaroyce.12@gmail.com>
2022-06-09 22:15:47 +0530

    initial

---
+++ utils.js
@@ -0,0 +1,11 @@
+const debounce = (func, delay) => {
+    let timeOutId;
+    return (...args) => {
+        if (timeOutId) {
+            clearTimeout(timeOutId)
+        }
+        timeOutId = setTimeout( () => {
+            func.apply(null, args)
+        }, delay)
+    }
+}
